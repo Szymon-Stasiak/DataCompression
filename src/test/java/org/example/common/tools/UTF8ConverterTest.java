606bae687b9987fcbc991519988d4d6695cb678b
Szymon <stszymek@gmail.com>
2024-12-20 23:19:19 +0100

    Tests for Utf8 converter

--- src/test/java/org/example/common/tools/UTF8ConverterTest.java
+++ src/test/java/org/example/common/tools/UTF8ConverterTest.java
@@ -1,5 +1,6 @@
 package org.example.common.tools;
 
+import static org.example.common.tools.UTF8Converter.convertBinaryToInt;
 import static org.example.common.tools.UTF8Converter.convertIntToUTF8;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -35,5 +36,50 @@ class UTF8ConverterTest {
     void should_properly_convertSmileCharacterToBinary() {
         String result = convertIntToUTF8(128578);
         assertEquals("11110000100111111001100110000010", result);
+    }
+
+    @Test
+    void should_throwErrorWhenIntIsGreaterThanMaxValue() {
+        assertThrows(IllegalArgumentException.class, () -> convertIntToUTF8(1114112));
+    }
+
+    @Test
+    void should_properly_convert1ByteStringToIntToUTF8() {
+        int result = convertBinaryToInt("01000001");
+        assertEquals(65, result);
+    }
+
+    @Test
+    void should_properly_convert2ByteStringToIntToUTF8() {
+        int result = convertBinaryToInt("1100010010111101");
+        assertEquals(317, result);
+    }
+
+    @Test
+    void should_properly_convert3ByteStringToIntToUTF8() {
+        int result = convertBinaryToInt("111000001011010110001111");
+        assertEquals(3407, result);
+    }
+
+    @Test
+    void should_properly_convert4ByteStringToIntToUTF8() {
+        int result = convertBinaryToInt("11110000100100001000110110001000");
+        assertEquals(66376, result);
+    }
+
+    @Test
+    void should_properly_convertSmileStringToIntToUTF8() {
+        int result = convertBinaryToInt("11110000100111111001100110000010");
+        assertEquals(128578, result);
+    }
+
+    @Test
+    void should_throwErrorWhenStringRepresentNotExistingCHar() {
+        assertThrows(IllegalArgumentException.class, () -> convertBinaryToInt("1111111111"));
+    }
+
+    @Test
+    void should_throwErrorWhenStringRepresentInvalidContinuationByte() {
+        assertThrows(IllegalArgumentException.class, () -> convertBinaryToInt("11060000"));
     }
 }
